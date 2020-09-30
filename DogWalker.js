794cc2308eb0fea8a16c64098525f9c6799c9333
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-09-30 15:04:20 -0400

    problem 4 js solution using an object literal to record frequency of each size of dog

--- DogWalker.js
+++ DogWalker.js
@@ -1,9 +1,24 @@
 class DogWalker {
-    numberOfWalksCalculator(dogSizes){
-        let numberOfWalks = -1;
-
-        return numberOfWalks;
+  numberOfWalksCalculator(dogSizes) {
+    //Initiate numberOfWalks as a counter and object literal to record frequency of each size
+    let dogSizeInventory = {};
+    let numberOfWalks = 0;
+    //Loop through given dogSizes array and populate dogSizeInventory object
+    for (let dogSize of dogSizes) {
+      if (!dogSizeInventory[dogSize]) {
+        dogSizeInventory[dogSize] = 1;
+      } else {
+        dogSizeInventory[dogSize]++;
+      }
     }
+    //Look up the frequency of each size
+    for (let size in dogSizeInventory) {
+      //Divide each size's frequency by 2, round up in case the division is not even and add that value to the number of walks
+      numberOfWalks += Math.ceil(dogSizeInventory[size] / 2);
+    }
+    console.log(numberOfWalks);
+    return numberOfWalks;
+  }
 }
 
 module.exports = DogWalker;
